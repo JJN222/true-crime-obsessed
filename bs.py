41907480e67ba8c7407bdce07d2cf7cfb55db803
JJN222 <naylor.jared@gmail.com>
2025-08-25 11:07:50 -0700

    Remove duplicate Wikipedia code causing NameError

--- bs.py
+++ bs.py
@@ -1128,54 +1128,6 @@ def search_wikidata(query, limit=25):
     
     return []
 
-# Also update the Wikipedia tab to be more helpful:
-with source_tabs[4]:  # Wikipedia
-    # Try Wikidata first
-    if wikidata_results:
-        st.markdown("#### Wikidata Entries")
-        for item in wikidata_results[:5]:
-            st.write(f"**{item['label']}**")
-            st.caption(f"{item['description']}")
-            st.write(f"[View on Wikidata]({item['url']})")
-            # Add Wikipedia link
-            wiki_url = f"https://en.wikipedia.org/wiki/{item['label'].replace(' ', '_')}"
-            st.write(f"[Search Wikipedia]({wiki_url})")
-            st.divider()
-    
-    # Always also try Wikipedia's own search API
-    st.markdown("#### Wikipedia Articles")
-    wiki_api_url = "https://en.wikipedia.org/w/api.php"
-    params = {
-        "action": "opensearch",
-        "search": case_search,
-        "limit": 5,
-        "format": "json"
-    }
-    
-    try:
-        response = requests.get(wiki_api_url, params=params, timeout=10)
-        if response.status_code == 200:
-            data = response.json()
-            if len(data) > 1 and data[1]:  # data[1] contains titles
-                for i, title in enumerate(data[1][:5]):
-                    # data[2] has descriptions, data[3] has URLs
-                    description = data[2][i] if len(data) > 2 and i < len(data[2]) else ""
-                    url = data[3][i] if len(data) > 3 and i < len(data[3]) else f"https://en.wikipedia.org/wiki/{title.replace(' ', '_')}"
-                    
-                    st.write(f"**{title}**")
-                    if description:
-                        st.caption(description)
-                    st.write(f"[Read on Wikipedia]({url})")
-                    st.divider()
-            else:
-                st.info("No Wikipedia articles found")
-    except Exception as e:
-        st.info("Wikipedia search unavailable")
-        # Provide direct search link
-        wiki_search_url = f"https://en.wikipedia.org/w/index.php?search={case_search.replace(' ', '+')}"
-        st.markdown(f"[Search Wikipedia directly]({wiki_search_url})")
-
-
 def get_wikipedia_content(article_title, max_chars=3000):
     """Fetch actual Wikipedia article content"""
     import urllib.parse
